83c3ff3f3612ddf5942d65f3dd6efc9b453efb88
valu <valu@163.com>
2017-05-14 11:06:00 +0800

    * mac_net_test.models.csproj: add models
    * test_model.cs:

--- mac_net_test.models/test_model.cs
+++ mac_net_test.models/test_model.cs
@@ -3,10 +3,22 @@ namespace mac_net_test.models
 {
     public class test_model
     {
-        public int id { get; set; }
+        public int id
+        {
+            get;
+            set;
+        }
 
-        public string uid { get; set; }
+        public string uid
+        {
+            get;
+            set;
+        }
 
-        public string pwd { get; set; }
+        public string pwd
+        {
+            get;
+            set;
+        }
     }
 }
