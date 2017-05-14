c5f57d44d9609e9e6935f9fb4ab0d065a665a111
valu <mzj@jundie.net>
2017-05-14 10:53:44 +0800

    update

---
+++ mac_net_test.models/test_model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace mac_net_test.models
+{
+    public class test_model
+    {
+        public int id { get; set; }
+
+        public string uid { get; set; }
+
+        public string pwd { get; set; }
+    }
+}
